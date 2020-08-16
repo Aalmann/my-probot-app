258623d5109b8837bf8791a1ffcfbcb3a438f650
Alexander Hanl <Aalmann@users.noreply.github.com>
2020-08-16 13:21:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ module.exports = app => {
   })
   app.on('issue_comment.edited', async context => {
     if (!context.isBot) {
-      const params = context.issue({ body: 'TYou have edited one comment.' })
+      const params = context.issue({ body: 'You have edited one comment.' })
 
       return context.github.issues.createComment(params)
     }
@@ -39,7 +39,7 @@ module.exports = app => {
     // `context` extracts information from the event, which can be passed to
     // GitHub API calls. This will return:
     //   { owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World! }
-    const params = context.issue({ body: 'Hello World again!' })
+    const params = context.issue({ body: 'Thanks for creating this issue.\nOne of the team members will give you feedback as soon as possible.' })
 
     // Post a comment on the issue
     return context.github.issues.createComment(params)
